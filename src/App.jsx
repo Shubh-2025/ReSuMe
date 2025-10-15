eff226c102e66c3200a1a70115f31879d62971b4
Shubhranil Karmakar <samratkarmakarnaihati@gmail.com>
2025-10-15 22:05:35 +0530

    tailwind configuration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>App</div>
-    </>
+    <div className='w-screen h-screen bg-amber-100 flex justify-center place-items-center' >
+      <div className='text-black font-extrabold text-4xl'>App</div>
+    </div>
   )
 }
 
-export default App
+export default App;
